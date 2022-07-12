6805bbd2849aaf6ebdb3048d394345688ea5f236
dTharuka <dakshinatharuka60@gmail.com>
2022-07-12 09:29:59 +0530

    Create Student UI

---
+++ src/controller/StudentFormController.java
@@ -0,0 +1,20 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+
+public class StudentFormController {
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtNic;
+    public Label lblId;
+    public TableColumn colId;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+}
