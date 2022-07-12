2a945711ee47aa6b790e831be677bd1f0027ca52
dTharuka <dakshinatharuka60@gmail.com>
2022-07-12 11:12:44 +0530

    Create DBConnection

---
+++ src/db/DBConnection.java
@@ -0,0 +1,29 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+    public class DBConnection {
+
+        private static DBConnection dbConnection = null;
+        private Connection connection;
+
+
+        private DBConnection() throws ClassNotFoundException, SQLException {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3307/ThogaKade", "root", "1234");
+        }
+
+
+        public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+
+            return (dbConnection==null)? dbConnection= new DBConnection(): dbConnection;
+        }
+
+        public Connection getConnection() {
+            return connection;
+        }
+    }
+
+
