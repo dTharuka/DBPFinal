24562373e5c6f52df98a417f07656fec1d218cd0
dTharuka <dakshinatharuka60@gmail.com>
2022-07-12 11:30:28 +0530

    Create LoadAllStudent

--- src/controller/StudentFormController.java
+++ src/controller/StudentFormController.java
@@ -1,12 +1,21 @@
 package controller;
 
 import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import model.student;
+import util.CrudUtil;
 
-public class StudentFormController {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class StudentFormController{
     public JFXTextField txtContact;
     public JFXTextField txtAddress;
     public JFXTextField txtName;
@@ -23,6 +32,7 @@ public class StudentFormController {
     public Button btnSave;
     public Button btnDelete;
     public JFXTextField txtSearch;
+    public TableView tblStudent;
 
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
     }
@@ -34,5 +44,40 @@ public class StudentFormController {
     }
 
     public void SearchStudentOnAction(ActionEvent actionEvent) {
+    }
+    public void initialize(){
+
+        colId.setCellValueFactory(new PropertyValueFactory("Id"));
+        colName.setCellValueFactory(new PropertyValueFactory("name"));
+        colEmail.setCellValueFactory(new PropertyValueFactory("email"));
+        colContact.setCellValueFactory(new PropertyValueFactory("contact"));
+        colAddress.setCellValueFactory(new PropertyValueFactory("address"));
+        colNic.setCellValueFactory(new PropertyValueFactory("nic"));
+
+        try {
+            loadAllCustomers();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadAllCustomers() throws ClassNotFoundException, SQLException {
+        ResultSet result = CrudUtil.execute("SELECT * FROM Student");
+        ObservableList<student> obList = FXCollections.observableArrayList();
+
+        while (result.next()){
+            obList.add(
+                    new student(
+                            result.getString("studentId"),
+                            result.getString("studentName"),
+                            result.getString("email"),
+                            result.getString("contact"),
+                            result.getString("address"),
+                            result.getString("nic")
+                    )
+            );
+        }
+        tblStudent.setItems(obList);
+
     }
 }
